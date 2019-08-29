44876fcf43e3c2ed683b613ed4232a4a3ec666be
Noveria <little.noveria@gmail.com>
2019-08-30 00:07:14 +0300

    создала проект для второй задачи

---
+++ Lesson1/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) 
+             * по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах.*/
+
+        }
+    }
+}
