82834ec4651259610075558b12667245bb2c4076
Noveria <little.noveria@gmail.com>
2019-08-30 00:20:01 +0300

    добавила решение второй задачи

--- Lesson1/Task2/Program.cs
+++ Lesson1/Task2/Program.cs
@@ -12,7 +12,17 @@ namespace Task2
         {
             /*Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) 
              * по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах.*/
+            double I;
+            Console.Write("Введите массу тела: ");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите рост человека: ");
+            double h = Convert.ToDouble(Console.ReadLine());
 
+            I = m / (h * h);
+
+            Console.WriteLine($"Индекс массы тела I = {I}");
+            
+            Console.ReadKey();
         }
     }
 }
