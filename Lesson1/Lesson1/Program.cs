6b3cadf7e1cc00cedab8bdf1e0b33dd4595e2fac
Noveria <little.noveria@gmail.com>
2019-08-29 23:37:55 +0300

    добавила пространство имен и решение задачи методом склеивания

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace Task1
 {
-    class Program
+    class Profile
     {
         static void Main(string[] args)
         {
+            /*Написать программу «Анкета». Последовательно задаются вопросы 
+             * (имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку:
+            а) используя  склеивание;
+	        б) используя форматированный вывод;
+	        в) используя вывод со знаком $.*/
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите вашу фамилию: ");
+            string surname = Console.ReadLine();
+            Console.Write("Сколько вам лет? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите ваш рост: ");
+            int growth = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите ваш вес: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
 
+            Console.Write("Имя:" + name + " Фамилия:" + surname + " Возраст:" + age + " Рост:" + growth + " Вес:" + weight);
+
+            Console.ReadKey();
         }
     }
 }
