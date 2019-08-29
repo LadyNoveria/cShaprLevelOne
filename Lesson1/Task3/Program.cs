c9c5231174fea8b668ba4ca0fce8e0092386a8c9
Noveria <little.noveria@gmail.com>
2019-08-30 00:27:46 +0300

    Создала проект для задачи 3

---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class DistanceCalc
+    {
+        static void Main(string[] args)
+        {
+            /*а) Написать программу, которая подсчитывает расстояние между точками с 
+             * координатами x1, y1 и x2,y2 по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2). 
+             * Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой);
+            б) *Выполнить предыдущее задание, оформив вычисления расстояния между точками в виде метода.*/
+
+
+        }
+    }
+}
