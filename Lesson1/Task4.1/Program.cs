1b1220dc9fed09c26346cf149bf61301e8e36e65
Noveria <little.noveria@gmail.com>
2019-08-30 01:08:36 +0300

    Добавила решение 4 задачи с использованием трех переменных

--- Lesson1/Task4.1/Program.cs
+++ Lesson1/Task4.1/Program.cs
@@ -12,7 +12,19 @@ namespace Task4._1
         {
             /*Написать программу обмена значениями двух переменных:
             а) с использованием третьей переменной.*/
+            string c;
+            Console.Write("Введите значение переменной a: ");
+            string a = Console.ReadLine();
+            Console.Write("Введите значение переменной b: ");
+            string b = Console.ReadLine();
 
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("a:" + a + " b:" + b);
+
+            Console.ReadKey();
 
         }
     }
