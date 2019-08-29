99340385d40bb6bc23ed7999235f6e38b8879929
Noveria <little.noveria@gmail.com>
2019-08-30 00:50:30 +0300

    Добавила проект для задачи 4 с применением трех переменных

---
+++ Lesson1/Task4.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4._1
+{
+    class Exchange
+    {
+        static void Main(string[] args)
+        {
+            /*Написать программу обмена значениями двух переменных:
+            а) с использованием третьей переменной.*/
+
+
+        }
+    }
+}
