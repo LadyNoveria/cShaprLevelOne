3c18340c539d11c067a17eb5badf941b79816587
Noveria <little.noveria@gmail.com>
2019-08-30 01:10:59 +0300

    Создала проект для решения 4 задачи с применением двух переменных

---
+++ Lesson1/Task4.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Написать программу обмена значениями двух переменных:
+            а)  *без использования третьей переменной.*/
+            
+            Console.Write("Введите значение переменной a: ");
+            string a = Console.ReadLine();
+            Console.Write("Введите значение переменной b: ");
+            string b = Console.ReadLine();
+
+            Console.WriteLine("a:" + a + " b:" + b);
+
+            Console.ReadKey();
+        }
+    }
+}
