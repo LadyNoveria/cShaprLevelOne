1857d7f7f7ec18f8f675d6eb97e5bb3a430b9b0e
Noveria <little.noveria@gmail.com>
2019-09-03 17:44:09 +0300

    решение второй задачи для чисел int

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Counter
+    {
+        static void Main(string[] args)
+        {
+            //Вяльшина Вероника
+            /*2. Написать метод подсчета количества цифр числа.*/
+
+            Console.Write("Enter number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digits in number: " + counter(Math.Abs(number)));
+            Console.ReadKey();
+        }
+
+        public static int counter(int x) {
+            int count = 0;
+            while (x > 0) {
+                x = x / 10;
+                count++;
+            }
+            return count;
+        }
+    }
+}
