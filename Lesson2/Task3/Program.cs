2bde39fca5fe0601bc6ee483e3cc547ea5df1fd0
Noveria <little.noveria@gmail.com>
2019-09-03 17:59:13 +0300

    решение третьей задачи

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вероника Вяльшина
+            /*3. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.*/
+            int number, result = 0;
+            while (true)
+            {
+                Console.Write("Enter number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number % 2 != 0 && number > 0){
+                    result = result + number;
+                }
+                if (number == 0) {
+                    Console.WriteLine("Сумма всех нечетных положительных чисел: " + result);
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
