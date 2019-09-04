6345df10cf6695e59816ebaf558965095e816f95
Noveria <little.noveria@gmail.com>
2019-09-04 21:30:53 +0300

    решение седьмой задачи, пункт b

---
+++ Lesson2/Task7.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вяльшина Вероника
+            /*7. a) Разработать рекурсивный метод, который выводит на экран числа от a до b (a<b);
+            б) *Разработать рекурсивный метод, который считает сумму чисел от a до b.*/
+
+            Console.Write("Введите число a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int amount = 0;
+            culcAmount(a, b, ref amount);
+
+            Console.WriteLine("Сумма чисел от a до b: " + amount);
+            Console.ReadKey();
+        }
+
+        public static void culcAmount(int a, int b, ref int amount) {
+            amount = amount + a;
+            if (a < b)
+                culcAmount(a + 1, b, ref amount);
+        }
+
+    }
+}
