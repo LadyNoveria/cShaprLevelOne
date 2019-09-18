99bd54d6daca5befd140b8c9c996d9db3a0e082f
Noveria <little.noveria@gmail.com>
2019-09-18 18:04:28 +0300

    добавлено решение задачи 2, пункт г метод искусственного формирования новой строки с пробелами вынесен в отдельный метод

--- Lesson5/Task2_1/Program.cs
+++ Lesson5/Task2_1/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 
+
 namespace Task2_1
 {
     #region Class Message
@@ -19,88 +20,89 @@ namespace Task2_1
             this.msg = msg;
         }
 
+        /// <summary>
+        /// Формирование строки самых длинных слов с помощью StringBuilder
+        /// </summary>
+        /// <returns></returns>
+        public StringBuilder stringOfLongestWords() {
+            StringBuilder strmax = new StringBuilder();
+            string[] max = findLongestWord();
+            for (int i = 0; i < max.Length; i++) {
+                strmax.Append($"{max[i]} ");
+            }
+
+            return strmax;
+        }
 
         /// <summary>
         /// Нахождение самого длинного слова в строке
         /// </summary>
         /// <returns></returns>
-        public string[] findLongestWord() {
-            string str1 = "";
-            for (int i = 0; i < msg.Length; i++)
-            {
-                if (@".,!?-".IndexOf(msg[i]) != -1)
-                    str1 += $" {msg[i]} ";
-                else
-                    str1 += $"{msg[i]}";
-            }
-
+        public string[] findLongestWord()
+        {
+            string str1 = NewLineForming();
             string[] str2 = str1.Split(' ');
             string tmp = str2[0];
-            for (int i = 0; i < str2.Length; i++) {
-                if (str2[i].Length > tmp.Length) {
-                    tmp = str2[i];
-                }
-            }
-
-            string[] max = new string[1];
             for (int i = 0; i < str2.Length; i++)
             {
-                if (str2[i].Length == tmp.Length) {
-                    Array.Resize(ref max, max.Length+1);
+                if (str2[i].Length > tmp.Length) tmp = str2[i];
+            }
+            string[] max = new string[0];
+            for (int i = 0; i < str2.Length; i++)
+            {
+                if (str2[i].Length == tmp.Length)
+                {
+                    Array.Resize(ref max, max.Length + 1);
                     max[max.Length - 1] = str2[i];
                 }
             }
-
             return max;
         }
 
+        /// <summary>
+        /// Формирование новой строки
+        /// </summary>
+        /// <returns></returns>
+        private string NewLineForming()
+        {
+            string str1 = "";
+            for (int i = 0; i < msg.Length; i++)
+            {
+                if (@".,!?-".IndexOf(msg[i]) != -1) str1 += $" {msg[i]} ";
+                else str1 += $"{msg[i]}";
+            }
+
+            return str1;
+        }
 
         /// <summary>
         /// удаление слов, которые заканчиваются на заданный символ
         /// </summary>
         public void deleteWord(char ch) {
-            string str1 = "";
-
-            for (int i = 0; i < msg.Length; i++) {
-                if (@".,!?-".IndexOf(msg[i]) != -1)
-                    str1 += $" {msg[i]} ";
-                else
-                    str1 += $"{msg[i]}";
-            }
-
+            string str1 = NewLineForming();
             var str2 = str1.Split(' ');
-            msg = "";
+            string str3 = "";
             foreach (var e in str2) {
-                if(@".,!?-".IndexOf(e) != -1)
-                    msg += $"{e}";
-                else if(e != "" && e[e.Length - 1] != ch)
-                    msg += $"{e} ";
+                if(@".,!?-".IndexOf(e) != -1) str3 += $"{e}";
+                else if(e != "" && e[e.Length - 1] != ch) str3 += $"{e} ";
             }
             //дописать
-            Console.WriteLine($"msg: {msg}");
+            Console.WriteLine($"msg: {str3}");
         }
         /// <summary>
         /// Вывод сообщений, содержащих не более n символов
         /// </summary> 
         /// <param name="n"></param>
         public void consoleLimInLength(int n) {
-            string str1 = "";
-            for (int i = 0; i < msg.Length; i++)
-            {
-                if (@".,!?-".IndexOf(msg[i]) != -1)
-                    str1 += $" {msg[i]} ";
-                else
-                    str1 += $"{msg[i]}";
-            }
+            string str1 = NewLineForming();
 
-            string[] str2 = msg.Split(' ');
+            string[] str2 = str1.Split(' ');
             string pattern = @"[a-zA-Z]{" + (n) + "}";
             Regex regex = new Regex(pattern);
 
             for (int i = 0; i < str2.Length;i++)
             {
-                if (regex.IsMatch(str2[i]) && str2[i].Length == n)
-                    Console.WriteLine($">>> {str2[i]}");
+                if (regex.IsMatch(str2[i]) && str2[i].Length == n) Console.WriteLine($">>> {str2[i]}");
             }
         }
     }
@@ -131,6 +133,7 @@ namespace Task2_1
                 Console.WriteLine($">>> {LongestWord[i]}");
             }
 
+            Console.WriteLine($"String Of Longest Words: {msg.stringOfLongestWords()}"); 
 
             Pause();
         }
